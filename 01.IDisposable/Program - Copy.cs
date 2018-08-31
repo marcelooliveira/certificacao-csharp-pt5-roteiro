581a2b221d6589c772b8be80c1c0211c2891816e
marcelooliveira <mclricardo@gmail.com>
2018-08-31 14:50:19 -0300

    Demonstração de garbage collector em ação

---
+++ 01.IDisposable/Program - Copy.cs
@@ -0,0 +1,80 @@
+﻿//using System;
+//using System.Diagnostics;
+
+//namespace _01.IDisposable
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Cliente cliente = new Cliente();
+
+//            Third t = new Third();
+//            Console.ReadKey();
+//        }
+
+//        public class Cliente
+//        {
+//            public Cliente()
+//            {
+//                ClasseComFinalizador obj = new ClasseComFinalizador();
+//                obj.MostrarDuracao();
+//            }
+//        }
+
+//        public class ClasseComFinalizador
+//        {
+//            Stopwatch sw;
+
+//            public ClasseComFinalizador()
+//            {
+//                sw = Stopwatch.StartNew();
+//                Console.WriteLine("Objeto instanciado.");
+//            }
+
+//            public void MostrarDuracao()
+//            {
+//                Console.WriteLine("A instância {0} existe há {1} milissegundos.",
+//                                  this.GetType(), sw.Elapsed.TotalMilliseconds);
+//            }
+
+//            ~ClasseComFinalizador()
+//            {
+//                Console.WriteLine("Finalizando o objeto");
+//                sw.Stop();
+//                Console.WriteLine("A instância {0} existiu por {1} milissegundos.",
+//                                  this.GetType(), sw.Elapsed.TotalMilliseconds);
+//            }
+//        }
+//    }
+
+//    class First
+//    {
+//        ~First()
+//        {
+//            System.Diagnostics.Trace.WriteLine("First's destructor is called.");
+//        }
+//    }
+
+//    class Second : First
+//    {
+//        ~Second()
+//        {
+//            System.Diagnostics.Trace.WriteLine("Second's destructor is called.");
+//        }
+//    }
+
+//    class Third : Second
+//    {
+//        ~Third()
+//        {
+//            System.Diagnostics.Trace.WriteLine("Third's destructor is called.");
+//        }
+//    }
+
+//    /* Output (to VS Output Window):
+//        Third's destructor is called.
+//        Second's destructor is called.
+//        First's destructor is called.
+//    */
+//}
--- 01.IDisposable/Program.cs
+++ 01.IDisposable/Program.cs
@@ -1,80 +1,28 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace _01.IDisposable
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Cliente cliente = new Cliente();
+            await Task.Delay(3000); //aguarda 3 segundos
+            for (long i = 0; i < 100000000000; i++)
+            {
+                Livro l = new Livro();
+                await Task.Delay(3); //aguarda 3 milissegundos
+            }
 
-            Third t = new Third();
             Console.ReadKey();
         }
-
-        public class Cliente
-        {
-            public Cliente()
-            {
-                ClasseComFinalizador obj = new ClasseComFinalizador();
-                obj.MostrarDuracao();
-            }
-        }
-
-        public class ClasseComFinalizador
-        {
-            Stopwatch sw;
-
-            public ClasseComFinalizador()
-            {
-                sw = Stopwatch.StartNew();
-                Console.WriteLine("Objeto instanciado.");
-            }
-
-            public void MostrarDuracao()
-            {
-                Console.WriteLine("A instância {0} existe há {1} milissegundos.",
-                                  this.GetType(), sw.Elapsed.TotalMilliseconds);
-            }
-
-            ~ClasseComFinalizador()
-            {
-                Console.WriteLine("Finalizando o objeto");
-                sw.Stop();
-                Console.WriteLine("A instância {0} existiu por {1} milissegundos.",
-                                  this.GetType(), sw.Elapsed.TotalMilliseconds);
-            }
-        }
     }
 
-    class First
+    class Livro
     {
-        ~First()
-        {
-            System.Diagnostics.Trace.WriteLine("First's destructor is called.");
-        }
+        string[] palavras = new string[1000000];
     }
 
-    class Second : First
-    {
-        ~Second()
-        {
-            System.Diagnostics.Trace.WriteLine("Second's destructor is called.");
-        }
-    }
-
-    class Third : Second
-    {
-        ~Third()
-        {
-            System.Diagnostics.Trace.WriteLine("Third's destructor is called.");
-        }
-    }
-
-    /* Output (to VS Output Window):
-        Third's destructor is called.
-        Second's destructor is called.
-        First's destructor is called.
-    */
+    //<image url="$(ProjectDir)\img1.png"/>
 }
