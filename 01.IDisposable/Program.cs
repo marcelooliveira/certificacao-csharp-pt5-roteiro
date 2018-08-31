92c42c3e7c9384a13ceb542a241851e7e3477886
marcelooliveira <mclricardo@gmail.com>
2018-08-31 17:07:43 -0300

    Dispose, GC.SuppressFinalize

--- 01.IDisposable/Program.cs
+++ 01.IDisposable/Program.cs
@@ -3,84 +3,56 @@ using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace _01.IDisposable
+namespace _01.idisposable
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
-            await Task.Delay(3000); //aguarda 3 segundos
-
-            await GerarTiposValor();
-
-            //await GerarTiposReferencia();
-            //Quando são gerados muitos objetos,
-            //a imagem abaixo mostra que a quantidade de memória alocada
-            //aumenta bastante após 3 segundos, mas se mantém estável, pois
-            //nesse momento o coletor de lixo age para 
-            //liberar os objetos que deixaram de ser utilizados
-            //<image url="$(ProjectDir)\img2.png"/>
-
-            Console.ReadKey();
-        }
-
-        private static async Task GerarTiposValor()
-        {
-            for (long i = 0; i < 100000000000; i++)
-            {
-                byte nivelDeAzul = 0xFF; // == System.Byte 
-                int populacao = 1500; // System.Int32 
-                short passageirosVoo = 230; // System.Int16 
-                long populacaoDoBrasil = 207_660_929; // System.Int64 
-
-                sbyte nivelDeBrilho = 127; // System.Sbyte 
-                uint estoque = 1500; // System.UInt32 
-                ushort passageirosNavio = 230; // System.UInt16 
-                ulong populacaoDoMundo = 7_000_000_000; // System.UInt64 
-
-                bool fumante = false; // System.Boolean booleano
-                decimal salario = 5000m; // System.Decimal 
-                double massaDaTerra = 5.9736e24; // System.Double
-                float numeroPI = 3.14159f; // System.Single
-                await Task.Delay(3); //aguarda 3 milissegundos
-            }
-
-            //a imagem abaixo mostra que a quantidade de memória alocada
-            //não varia significativamente
-            //<image url="$(ProjectDir)\img1.png"/>
-        }
-
-        private static async Task GerarTiposReferencia()
-        {
-            for (long i = 0; i < 100000000000; i++)
-            {
-                Livro l = new Livro();
-                await Task.Delay(3); //aguarda 3 milissegundos
-            }
+            ResourceHolder r = new ResourceHolder();
+            r.Dispose();
         }
     }
 
-    class Livro
+    //IDisposable: Fornece um mecanismo para liberar recursos não gerenciados.
+    class ResourceHolder : IDisposable
     {
-        string[] palavras = new string[1000000];
+        // Indica quando um objeto foi descartado
+        bool descartado = false;
 
-        ~Livro()
+
+        /// <summary>
+        /// Executa tarefas definidas pelo aplicativo associadas à liberação,
+        /// liberação ou redefinição de recursos não gerenciados.
+        /// </summary>
+        public void Dispose()
         {
-            //O finalizador é invocado pelo coletor de lixo
+            // Chama Dispose() e informa que a chamada
+            //foi feita durante o descarte do objeto
+            Descartar(true);
+            //Solicita que o common language runtime não chame o 
+            //finalizador para o objeto especificado
+            GC.SuppressFinalize(this);
+        }
 
-            //Aqui o livro deve ser finalizado
-            Console.WriteLine("Finalizador chamado...");
+        public virtual void Descartar(bool descartando)
+        {
+            // Abandona se o objeto já foi descartado
+            if (descartado)
+                return;
+            if (descartando)
+            {
+                // libera objetos gerenciados
+            }
+            // libera objetos não-gerenciados
+            descartado = true;
+        }
 
-            //CUIDADO AO HABILITAR O COMANDO SLEEP ABAIXO!
-            //============================================
-            //Sabotando o coletor de lixo...
-            //Assim, o número de lixo sem ser coletado se acumula...
-            Thread.Sleep(100);
-
-            //Note que agora a memória ocupada cresce absurdamente
-            //quando atrasamos a execução do coletor de lixo...
-            //<image url="$(ProjectDir)\img3.png"/>
-
+        ~ResourceHolder()
+        {
+            // descarta somente objetos não-gerenciados
+            Descartar(false);
         }
     }
+
 }
--- 03.Coleta/Program.cs
+++ 03.Coleta/Program.cs
@@ -1,12 +1,84 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace _03.Coleta
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            await Task.Delay(3000); //aguarda 3 segundos
+
+            await GerarTiposValor();
+
+            //await GerarTiposReferencia();
+            //Quando são gerados muitos objetos,
+            //a imagem abaixo mostra que a quantidade de memória alocada
+            //aumenta bastante após 3 segundos, mas se mantém estável, pois
+            //nesse momento o coletor de lixo age para 
+            //liberar os objetos que deixaram de ser utilizados
+            //<image url="$(ProjectDir)\img2.png"/>
+
+            Console.ReadKey();
+        }
+
+        private static async Task GerarTiposValor()
+        {
+            for (long i = 0; i < 100000000000; i++)
+            {
+                byte nivelDeAzul = 0xFF; // == System.Byte 
+                int populacao = 1500; // System.Int32 
+                short passageirosVoo = 230; // System.Int16 
+                long populacaoDoBrasil = 207_660_929; // System.Int64 
+
+                sbyte nivelDeBrilho = 127; // System.Sbyte 
+                uint estoque = 1500; // System.UInt32 
+                ushort passageirosNavio = 230; // System.UInt16 
+                ulong populacaoDoMundo = 7_000_000_000; // System.UInt64 
+
+                bool fumante = false; // System.Boolean booleano
+                decimal salario = 5000m; // System.Decimal 
+                double massaDaTerra = 5.9736e24; // System.Double
+                float numeroPI = 3.14159f; // System.Single
+                await Task.Delay(3); //aguarda 3 milissegundos
+            }
+
+            //a imagem abaixo mostra que a quantidade de memória alocada
+            //não varia significativamente
+            //<image url="$(ProjectDir)\img1.png"/>
+        }
+
+        private static async Task GerarTiposReferencia()
+        {
+            for (long i = 0; i < 100000000000; i++)
+            {
+                Livro l = new Livro();
+                await Task.Delay(3); //aguarda 3 milissegundos
+            }
+        }
+    }
+
+    class Livro
+    {
+        string[] palavras = new string[1000000];
+
+        ~Livro()
+        {
+            //O finalizador é invocado pelo coletor de lixo
+
+            //Aqui o livro deve ser finalizado
+            Console.WriteLine("Finalizador chamado...");
+
+            //Sabotando o coletor de lixo...
+            //Assim, o número de lixo sem ser coletado se acumula...
+
+            //CUIDADO AO HABILITAR O COMANDO SLEEP ABAIXO!
+            //Thread.Sleep(100);
+
+            //Note que agora a memória ocupada cresce absurdamente
+            //quando atrasamos a execução do coletor de lixo...
+            //<image url="$(ProjectDir)\img3.png"/>
+
         }
     }
 }
