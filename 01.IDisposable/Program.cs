f61d233889e7af6ece457ab7d0b4155a5454469e
marcelooliveira <mclricardo@gmail.com>
2018-08-31 15:33:31 -0300

    .

--- 01.IDisposable/Program.cs
+++ 01.IDisposable/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace _01.IDisposable
@@ -9,20 +10,77 @@ namespace _01.IDisposable
         static async Task Main(string[] args)
         {
             await Task.Delay(3000); //aguarda 3 segundos
+
+            await GerarTiposValor();
+
+            //await GerarTiposReferencia();
+            //Quando são gerados muitos objetos,
+            //a imagem abaixo mostra que a quantidade de memória alocada
+            //aumenta bastante após 3 segundos, mas se mantém estável, pois
+            //nesse momento o coletor de lixo age para 
+            //liberar os objetos que deixaram de ser utilizados
+            //<image url="$(ProjectDir)\img2.png"/>
+
+            Console.ReadKey();
+        }
+
+        private static async Task GerarTiposValor()
+        {
+            for (long i = 0; i < 100000000000; i++)
+            {
+                byte nivelDeAzul = 0xFF; // == System.Byte 
+                int populacao = 1500; // System.Int32 
+                short passageirosVoo = 230; // System.Int16 
+                long populacaoDoBrasil = 207_660_929; // System.Int64 
+
+                sbyte nivelDeBrilho = 127; // System.Sbyte 
+                uint estoque = 1500; // System.UInt32 
+                ushort passageirosNavio = 230; // System.UInt16 
+                ulong populacaoDoMundo = 7_000_000_000; // System.UInt64 
+
+                bool fumante = false; // System.Boolean booleano
+                decimal salario = 5000m; // System.Decimal 
+                double massaDaTerra = 5.9736e24; // System.Double
+                float numeroPI = 3.14159f; // System.Single
+                await Task.Delay(3); //aguarda 3 milissegundos
+            }
+
+            //a imagem abaixo mostra que a quantidade de memória alocada
+            //não varia significativamente
+            //<image url="$(ProjectDir)\img1.png"/>
+        }
+
+        private static async Task GerarTiposReferencia()
+        {
             for (long i = 0; i < 100000000000; i++)
             {
                 Livro l = new Livro();
                 await Task.Delay(3); //aguarda 3 milissegundos
             }
-
-            Console.ReadKey();
         }
     }
 
     class Livro
     {
         string[] palavras = new string[1000000];
-    }
 
-    //<image url="$(ProjectDir)\img1.png"/>
+        ~Livro()
+        {
+            //O finalizador é invocado pelo coletor de lixo
+
+            //Aqui o livro deve ser finalizado
+            Console.WriteLine("Finalizador chamado...");
+
+            //CUIDADO AO HABILITAR O COMANDO SLEEP ABAIXO!
+            //============================================
+            //Sabotando o coletor de lixo...
+            //Assim, o número de lixo sem ser coletado se acumula...
+            Thread.Sleep(100);
+
+            //Note que agora a memória ocupada cresce absurdamente
+            //quando atrasamos a execução do coletor de lixo...
+            //<image url="$(ProjectDir)\img3.png"/>
+
+        }
+    }
 }
